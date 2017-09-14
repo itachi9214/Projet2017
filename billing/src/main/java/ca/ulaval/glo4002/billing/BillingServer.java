f34d5f1cd5ed27aa4444f256468334fdc0e8ad6c
zakaria <sassiouizakaria@gmail.com>
2017-09-14 03:35:59 +0200

    Adding class billingServer

---
+++ billing/src/main/java/ca/ulaval/glo4002/billing/BillingServer.java
@@ -0,0 +1,35 @@
+package ca.ulaval.glo4002.billing;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.servlet.ServletContainer;
+
+public class BillingServer implements Runnable {
+	private static final int PORT = 8181;
+
+	public static void main(String[] args) {
+		new BillingServer().run();
+	}
+
+	@Override
+	public void run() {
+		Server server = new Server(PORT);
+		ServletContextHandler contextHandler = new ServletContextHandler(server, "/");
+		ResourceConfig packageConfig = new ResourceConfig().packages("ca.ulaval.glo4002.billing");
+		ServletContainer container = new ServletContainer(packageConfig);
+		ServletHolder servletHolder = new ServletHolder(container);
+
+		contextHandler.addServlet(servletHolder, "/*");
+
+		try {
+			server.start();
+			server.join();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			server.destroy();
+		}
+	}
+}

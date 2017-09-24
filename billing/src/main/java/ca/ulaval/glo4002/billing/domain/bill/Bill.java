e3b261d448be35ab6f3a926153d0e3d35f1d297f
aissatounovas <aissatou.diallo6@ulaval.ca>
2017-09-24 12:55:49 -0400

    refactor

--- billing/src/main/java/ca/ulaval/glo4002/billing/domain/bill/Bill.java
+++ billing/src/main/java/ca/ulaval/glo4002/billing/domain/bill/Bill.java
@@ -2,18 +2,20 @@ package ca.ulaval.glo4002.billing.domain.bill;
 
 import java.util.List;
 
+import ca.ulaval.glo4002.billing.domain.DueTerm;
+
 public class Bill {
   int billNumber;
   DueTerm dueTerm;
   int clientNumber;
   List<OrderedProduct> products;
-  float somme;
+  float billTotal;
 
-  public float Billcalculate() {
+  public float calculateBill() {
     for (OrderedProduct product : products) {
-      float somme = product.calculateProduct();
+      billTotal = product.calculateProduct();
     }
-    return somme;
+    return billTotal;
   }
 
 }

e3272d37036c7e21e3c5cfe85cb68c3009b117b3
aissatounovas <aissatou.diallo6@ulaval.ca>
2017-09-24 12:38:01 -0400

    create bill class,OrderedProduct and add dependency junit Mockito in pom.xml

---
+++ billing/src/test/java/ca/ulaval/glo4002/billing/domain/bill/OrderedProductTest.java
@@ -0,0 +1,29 @@
+package ca.ulaval.glo4002.billing.domain.bill;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class OrderedProductTest {
+  private int productId = 1;
+  private float productPrice = 100.00f;
+  private String productNote;
+  private int orderedProductQuantity = 2;
+  OrderedProduct orderedProduct;
+  Bill bill;
+
+  @Before
+  public void setUp() {
+    bill = new Bill();
+    orderedProduct = new OrderedProduct(productId, productPrice, productNote,
+        orderedProductQuantity);
+  }
+
+  @Test
+  public void WhencalculeProductThenReturnPrice() {
+    float result = orderedProduct.calculateProduct();
+    assertTrue(200.0 == result);
+
+  }
+}

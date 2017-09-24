7d65241ff9de88d3ba3944bd6812715611b4240a
marianne-butaye <marianne.butaye@gmail.com>
2017-09-24 13:16:55 -0400

    Correcting indent

--- billing/src/main/java/ca/ulaval/glo4002/billing/api/dto/ClientDto.java
+++ billing/src/main/java/ca/ulaval/glo4002/billing/api/dto/ClientDto.java
@@ -6,41 +6,41 @@ import ca.ulaval.glo4002.billing.domain.DueTerm;
 
 public class ClientDto {
 
-	private Integer clientNumber;
-	private Instant creationDate;
-	private DueTerm defaultTerm;
-	private String fullName;
+  private Integer clientNumber;
+  private Instant creationDate;
+  private DueTerm defaultTerm;
+  private String fullName;
 
-	public Integer getId() {
-		return clientNumber;
-	}
+  public Integer getId() {
+    return clientNumber;
+  }
 
-	public void setId(Integer clientNumber) {
-		this.clientNumber = clientNumber;
-	}
+  public void setId(Integer clientNumber) {
+    this.clientNumber = clientNumber;
+  }
 
-	public Instant getCreationDate() {
-		return creationDate;
-	}
+  public Instant getCreationDate() {
+    return creationDate;
+  }
 
-	public void setCreationDate(Instant creationDate) {
-		this.creationDate = creationDate;
-	}
+  public void setCreationDate(Instant creationDate) {
+    this.creationDate = creationDate;
+  }
 
-	public String getFullName() {
-		return fullName;
-	}
+  public String getFullName() {
+    return fullName;
+  }
 
-	public void setFullName(String fullName) {
-		this.fullName = fullName;
-	}
+  public void setFullName(String fullName) {
+    this.fullName = fullName;
+  }
 
-	public DueTerm getDefaultTerm() {
-		return defaultTerm;
-	}
+  public DueTerm getDefaultTerm() {
+    return defaultTerm;
+  }
 
-	public void setDefaultTerm(DueTerm defaultTerm) {
-		this.defaultTerm = defaultTerm;
-	}
+  public void setDefaultTerm(DueTerm defaultTerm) {
+    this.defaultTerm = defaultTerm;
+  }
 
 }

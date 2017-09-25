dc12edf0294aea95e3c81bed11ace93d473c0179
marianne-butaye <marianne.butaye@gmail.com>
2017-09-25 00:17:16 -0400

    Modifying for PR + added HTTP Codes & status to Resources

--- billing/src/main/java/ca/ulaval/glo4002/billing/api/BillingResource.java
+++ billing/src/main/java/ca/ulaval/glo4002/billing/api/BillingResource.java
@@ -5,9 +5,9 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import ca.ulaval.glo4002.billing.api.dto.RequestBillDto;
-import ca.ulaval.glo4002.billing.api.dto.ResponseBillDto;
 import ca.ulaval.glo4002.billing.domain.BillService;
 
 @Path("/bills")
@@ -26,8 +26,9 @@ public class BillingResource {
   @POST
   @Produces(MediaType.APPLICATION_JSON)
   @Consumes(MediaType.APPLICATION_JSON)
-  public ResponseBillDto createBill(RequestBillDto requestBillDto) {
-    return billService.createBill(requestBillDto);
+  public Response createBill(RequestBillDto requestBillDto) {
+    return Response.status(Response.Status.CREATED).entity(billService.createBill(requestBillDto))
+        .build();
   }
 
 }

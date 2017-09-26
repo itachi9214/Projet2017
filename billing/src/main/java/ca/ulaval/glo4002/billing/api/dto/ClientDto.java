02c4748793a4c9673a88d85caa17e5490b728ca3
marianne-butaye <marianne.butaye@gmail.com>
2017-09-26 19:27:51 -0400

    Ignoring unknown properties

--- billing/src/main/java/ca/ulaval/glo4002/billing/api/dto/ClientDto.java
+++ billing/src/main/java/ca/ulaval/glo4002/billing/api/dto/ClientDto.java
@@ -2,8 +2,11 @@ package ca.ulaval.glo4002.billing.api.dto;
 
 import java.time.Instant;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import ca.ulaval.glo4002.billing.domain.DueTerm;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ClientDto {
 
   private Long clientNumber;

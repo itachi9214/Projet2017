2efeaa64691acb488e1d1d25f9178ad2ad9f6d4e
marianne-butaye <marianne.butaye@gmail.com>
2017-09-26 20:31:17 -0400

    Using ObjectMapper to deserialize java.time.Instant

--- billing/src/main/java/ca/ulaval/glo4002/billing/api/CrmHttpClient.java
+++ billing/src/main/java/ca/ulaval/glo4002/billing/api/CrmHttpClient.java
@@ -1,9 +1,16 @@
 package ca.ulaval.glo4002.billing.api;
 
+import java.io.IOException;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import ca.ulaval.glo4002.billing.api.dto.ClientDto;
 import ca.ulaval.glo4002.billing.exception.NotFoundClientException;
@@ -18,13 +25,22 @@ public class CrmHttpClient extends HttpClient {
   @Produces(MediaType.APPLICATION_JSON)
   @Consumes(MediaType.APPLICATION_JSON)
   public ClientDto getClientDtoFromCrm(Long clientNumber) {
+    ObjectMapper mapper = new ObjectMapper().registerModule(new JavaTimeModule());
+    mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+
     String url = LOCALHOST + CLIENTS + clientNumber;
     Response response = callUrlWithGetMethod(url);
 
     if (response.getStatus() == HTTP_STATUS_NOT_FOUND)
       throw new NotFoundClientException(MESSAGE_NOT_FOUND);
 
-    ClientDto clientDto = response.readEntity(ClientDto.class);
+    ClientDto clientDto = null;
+    try {
+      clientDto = mapper.readValue(response.readEntity(JsonParser.class), ClientDto.class);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
     response.close();
     return clientDto;
   }

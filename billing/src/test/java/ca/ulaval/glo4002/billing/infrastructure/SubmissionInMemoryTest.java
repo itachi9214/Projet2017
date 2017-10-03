162e8a4832edcbc494689be0af95f2e836b8fb7e
khalloud siham <siham.khalloud.1@ulaval.ca>
2017-10-03 19:23:13 -0400

    added fonction that search if submission exists then correct createSubmission

--- billing/src/test/java/ca/ulaval/glo4002/billing/infrastructure/SubmissionInMemoryTest.java
+++ billing/src/test/java/ca/ulaval/glo4002/billing/infrastructure/SubmissionInMemoryTest.java
@@ -1,6 +1,7 @@
 package ca.ulaval.glo4002.billing.infrastructure;
 
-import static org.mockito.Mockito.verify;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -13,21 +14,32 @@ import ca.ulaval.glo4002.billing.domain.Submission.Submission;
 @RunWith(MockitoJUnitRunner.class)
 public class SubmissionInMemoryTest {
 
-  public SubmissionInMemory submissionInMemory;
+  private final static Long SUBMISSION_NUMBER = 100L;
+  private final static Long NOT_EXISTING_SUBMISSION_NUMBER = 200L;
+
+  private SubmissionInMemory submissionInMemory;
 
   @Mock
-  Submission submission;
+  Submission submissionMock;
 
   @Before
   public void setUp() {
     submissionInMemory = new SubmissionInMemory();
+    when(submissionMock.getBillNumber()).thenReturn(SUBMISSION_NUMBER);
   }
 
   @Test
-  public void givenSubmissionWhenCreateSubmissionPutSubmission() {
-    submissionInMemory.createSubmission(submission);
+  public void givenSubmissionNumberWhenGetSubmissionThenReturnSubmission() {
+    submissionInMemory.createSubmission(submissionMock);
 
-    verify(submission).getClientId();
+    Submission submission = submissionInMemory.getSubmissionById(SUBMISSION_NUMBER);
+
+    assertEquals(submission, submissionMock);
+  }
+
+  @Test(expected = NotFoundSubmissionException.class)
+  public void withWrongSubmissionNumberWhenGetSubmissionThenThrowException() {
+    submissionInMemory.getSubmissionById(NOT_EXISTING_SUBMISSION_NUMBER);
   }
 
 }

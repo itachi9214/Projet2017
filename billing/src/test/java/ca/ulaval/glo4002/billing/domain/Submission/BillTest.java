5a92af8d65510aa67faa6ee9d903521525402dbb
marianne-butaye <marianne.butaye@gmail.com>
2017-10-09 12:25:33 -0400

    Modifications after rebase

--- billing/src/test/java/ca/ulaval/glo4002/billing/domain/Submission/BillTest.java
+++ billing/src/test/java/ca/ulaval/glo4002/billing/domain/Submission/BillTest.java
@@ -2,19 +2,15 @@ package ca.ulaval.glo4002.billing.domain.Submission;
 
 import static org.junit.Assert.assertEquals;
 
-import java.time.LocalDateTime;
-
 import org.junit.Test;
 
 public class BillTest {
 
-  private LocalDateTime effectiveDate;
   private Bill bill;
 
   @Test
   public void givenDueTermWhenImmediateThenCalculateExpectedPaiementDate() {
     bill = new Bill(DueTerm.IMMEDIATE);
-    effectiveDate = bill.getEffectiveDate();
 
     assertEquals(bill.calculateExpectedPaiementDate(), bill.getEffectiveDate());
   }
@@ -22,7 +18,6 @@ public class BillTest {
   @Test
   public void givenDueTermWhenOneMonthThenCalculateExpectedPaiementDate() {
     bill = new Bill(DueTerm.DAYS30);
-    effectiveDate = bill.getEffectiveDate();
 
     assertEquals(bill.calculateExpectedPaiementDate(), bill.getEffectiveDate().plusMonths(1));
   }
@@ -30,7 +25,6 @@ public class BillTest {
   @Test
   public void givenDueTermWhenThreeMonthThenCalculateExpectedPaiementDate() {
     bill = new Bill(DueTerm.DAYS90);
-    effectiveDate = bill.getEffectiveDate();
 
     assertEquals(bill.calculateExpectedPaiementDate(), bill.getEffectiveDate().plusMonths(3));
   }

d166ab0c6c1088d2663199c6809b936d02412cf0
marianne-butaye <marianne.butaye@gmail.com>
2017-10-18 21:07:31 -0400

    Modified version + dates

--- billing/src/main/java/ca/ulaval/glo4002/billing/domain/bill/Bill.java
+++ billing/src/main/java/ca/ulaval/glo4002/billing/domain/bill/Bill.java
@@ -4,8 +4,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import javax.persistence.Entity;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 
 import ca.ulaval.glo4002.billing.domain.identity.Identity;
 import ca.ulaval.glo4002.billing.domain.submision.DueTerm;
@@ -16,9 +14,7 @@ import ca.ulaval.glo4002.billing.domain.submision.Submission;
 @Entity
 public class Bill extends Submission {
 
-  @Temporal(TemporalType.DATE)
   private LocalDateTime effectiveDate;
-  @Temporal(TemporalType.DATE)
   private LocalDateTime expectedPaiement;
 
   public Bill(Identity billNumber, DueTerm dueTerm, Long clientId, List<OrderedProduct> items)

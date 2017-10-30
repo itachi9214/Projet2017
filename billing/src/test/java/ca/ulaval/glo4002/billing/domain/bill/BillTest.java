0e93342532471f2672ec423e96d5592330ad7022
Rabab Moubine <moubine.rabab@gmail.com>
2017-10-30 18:19:44 -0400

    correction des noms dans BillTest

--- billing/src/test/java/ca/ulaval/glo4002/billing/domain/bill/BillTest.java
+++ billing/src/test/java/ca/ulaval/glo4002/billing/domain/bill/BillTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-import ca.ulaval.glo4002.billing.domain.bill.Bill;
 import ca.ulaval.glo4002.billing.domain.submision.DueTerm;
 
 public class BillTest {
@@ -12,21 +11,21 @@ public class BillTest {
   private Bill bill;
 
   @Test
-  public void givenDueTermWhenImmediateThenCalculateExpectedPaiementDate() {
+  public void givenDueTermWhenImmediateWhenCalculateExpectedPaiementDateThenIsEffectiveDate() {
     bill = new Bill(DueTerm.IMMEDIATE);
 
     assertEquals(bill.calculateExpectedPaiementDate(), bill.getEffectiveDate());
   }
 
   @Test
-  public void givenDueTermWhenOneMonthThenCalculateExpectedPaiementDate() {
+  public void givenDueTermWhenOneMonthWhenCalculateExpectedPaiementDateThenIsEffectiveDatePlusOneMonth() {
     bill = new Bill(DueTerm.DAYS30);
 
     assertEquals(bill.calculateExpectedPaiementDate(), bill.getEffectiveDate().plusMonths(1));
   }
 
   @Test
-  public void givenDueTermWhenThreeMonthThenCalculateExpectedPaiementDate() {
+  public void givenDueTermWhenThreeMonthWhenCalculateExpectedPaiementDateThenIsEffectiveDatePlusTreeMonths() {
     bill = new Bill(DueTerm.DAYS90);
 
     assertEquals(bill.calculateExpectedPaiementDate(), bill.getEffectiveDate().plusMonths(3));

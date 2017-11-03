38874fd99889009bb2ae0c79fe8ab2a5c3bf038a
itachi9214 <diakhateserignesaliou@gmail.com>
2017-11-02 21:53:45 -0300

    Refactor Class Test CrmHttpClientTest

--- billing/src/test/java/ca/ulaval/glo4002/billing/http/CrmHttpClientTest.java
+++ billing/src/test/java/ca/ulaval/glo4002/billing/http/CrmHttpClientTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertTrue;
 import static org.mockito.BDDMockito.willReturn;
 
 import java.io.IOException;
-import java.math.BigDecimal;
 import java.time.Instant;
 
 import javax.ws.rs.core.Response;
@@ -21,24 +20,22 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import ca.ulaval.glo4002.billing.api.dto.client.ClientDto;
-import ca.ulaval.glo4002.billing.api.dto.product.ProductDto;
 import ca.ulaval.glo4002.billing.domain.submision.DueTerm;
+import ca.ulaval.glo4002.billing.infrastructure.bill.CrmHttpClient;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CrmHttpClientTest {
 
-  private static final int AN_INT = 13;
   private static final String A_STRING = "name";
   private static final Long NON_EXISTING_CLIENT_NUMBER = -3L;
   private static final Long EXISTING_CLIENT_NUMBER = 1L;
-  private static final Integer NON_EXISTING_PRODUCT_ID = -6;
-  private static final Integer EXISTING_PRODUCT_ID = 3;
 
   private CrmHttpClient crmHttpClient;
   private ClientDto clientDto;
-  private ProductDto productDto;
   private ObjectMapper mapper;
 
+  @Mock
+  private Http http;
   @Mock
   private Response response;
 
@@ -48,17 +45,17 @@ public class CrmHttpClientTest {
     mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
 
     clientDto = new ClientDto(EXISTING_CLIENT_NUMBER, Instant.now(), DueTerm.IMMEDIATE, A_STRING);
-    productDto = new ProductDto(EXISTING_PRODUCT_ID, A_STRING, new BigDecimal(AN_INT));
-
-    crmHttpClient = new CrmHttpClient() {
-      protected Response callUrlWithGetMethod(String url) {
+    http = new Http() {
+      public Response callUrlWithGetMethod(String url) {
         return response;
       }
     };
+    crmHttpClient = new CrmHttpClient(http);
   }
 
   @Test(expected = ClientNotFoundException.class)
   public void givenClientNumberNotFoundWhenGetClientDtoThenThrowException() {
+
     willReturn(Status.NOT_FOUND.getStatusCode()).given(response).getStatus();
 
     crmHttpClient.getClientDto(NON_EXISTING_CLIENT_NUMBER);
@@ -72,23 +69,6 @@ public class CrmHttpClientTest {
     ClientDto result = crmHttpClient.getClientDto(EXISTING_CLIENT_NUMBER);
 
     assertTrue(result instanceof ClientDto);
-  }
-
-  @Test(expected = ProductNotFoundException.class)
-  public void givenProductIdNotFoundWhenGetProductDtoThenThrowException() {
-    willReturn(Status.NOT_FOUND.getStatusCode()).given(response).getStatus();
-
-    crmHttpClient.getProductDto(NON_EXISTING_PRODUCT_ID);
-  }
-
-  @Test
-  public void givenProductIdFoundWhenGetProductDtoThenReturnDto() throws IOException {
-    willReturn(Status.OK.getStatusCode()).given(response).getStatus();
-    willReturn(mapper.writeValueAsString(productDto)).given(response).readEntity(String.class);
-
-    ProductDto result = crmHttpClient.getProductDto(EXISTING_PRODUCT_ID);
-
-    assertTrue(result instanceof ProductDto);
   }
 
 }

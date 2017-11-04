b6cfe752501900647710392d5d828714856f00d7
Rabab Moubine <moubine.rabab@gmail.com>
2017-11-04 00:20:38 -0400

    ajout du httpPayment pour le post

--- billing/src/main/java/ca/ulaval/glo4002/billing/http/Http.java
+++ billing/src/main/java/ca/ulaval/glo4002/billing/http/Http.java
@@ -2,11 +2,14 @@ package ca.ulaval.glo4002.billing.http;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.glassfish.jersey.client.ClientProperties;
+
+import ca.ulaval.glo4002.billing.api.dto.payment.RequestPaymentDto;
 
 public class Http {
 
@@ -16,6 +19,15 @@ public class Http {
     client.property(ClientProperties.READ_TIMEOUT, 2000);
     WebTarget target = client.target(url);
     return target.request(MediaType.APPLICATION_JSON_TYPE).get();
+  }
+
+  public void callUrlWithPostMethod(String url, RequestPaymentDto requestPaymentDto) {
+    Entity<RequestPaymentDto> entity = Entity.entity(requestPaymentDto, MediaType.APPLICATION_JSON);
+    Client client = ClientBuilder.newClient();
+    client.property(ClientProperties.CONNECT_TIMEOUT, 2000);
+    client.property(ClientProperties.READ_TIMEOUT, 2000);
+    WebTarget target = client.target(url);
+    target.request(MediaType.APPLICATION_JSON_TYPE).post(entity);
   }
 
 }
--- billing/src/main/java/ca/ulaval/glo4002/billing/service/bill/BillService.java
+++ billing/src/main/java/ca/ulaval/glo4002/billing/service/bill/BillService.java
@@ -1,12 +1,9 @@
 package ca.ulaval.glo4002.billing.service.bill;
 
-import java.math.BigDecimal;
-
 import ca.ulaval.glo4002.billing.ServiceLocator;
 import ca.ulaval.glo4002.billing.api.dto.bill.BillDto;
 import ca.ulaval.glo4002.billing.domain.bill.Bill;
 import ca.ulaval.glo4002.billing.domain.bill.BillRepository;
-import ca.ulaval.glo4002.billing.domain.bill.BillState;
 import ca.ulaval.glo4002.billing.domain.identity.Identity;
 import ca.ulaval.glo4002.billing.domain.identity.IdentityFactory;
 import ca.ulaval.glo4002.billing.domain.submision.Submission;
@@ -45,27 +42,6 @@ public class BillService {
   }
 
   public void cancelBill(long billNumber) {
-    Identity identity = identityFactory.createIdFromNumber(billNumber);
-
-    Bill billToCancel = billRepository.findById(identity);
-    billToCancel.setBillState(BillState.PAID);
-    BigDecimal canceledBillPaidAmount = billToCancel.getPaidAmount();
-    if (canceledBillPaidAmount.compareTo(new BigDecimal(0)) == 1) {
-      payOtherBills(canceledBillPaidAmount, identity);
-    }
-  }
-
-  protected void payOtherBills(BigDecimal canceledBillPaidAmount, Identity identity) {
-    while (!canceledBillPaidAmount.equals(new BigDecimal(0))) {
-      Bill bill = billRepository.findOldestUnpaidBillOfSameClient(identity);
-      if (bill.calculateUnpaidAmount().compareTo(canceledBillPaidAmount) == 1) {
-        bill.setPaidAmount(canceledBillPaidAmount.add(bill.getPaidAmount()));
-        canceledBillPaidAmount = new BigDecimal(0);
-      } else {
-        bill.setPaidAmount(bill.getTotalPrice());
-        canceledBillPaidAmount = canceledBillPaidAmount.subtract(bill.calculateUnpaidAmount());
-      }
-    }
   }
 
 }

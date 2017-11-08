a4f4186bd0ae9463324124b44a7f01bc9778929a
khalloud siham <siham.khalloud.1@ulaval.ca>
2017-11-07 19:18:44 -0500

    supprimer doublon

--- payment/src/main/java/ca/ulaval/glo4002/payment/domaine/payment/Payment.java
+++
@@ -1,5 +0,0 @@
-package ca.ulaval.glo4002.payment.domaine.payment;
-
-public class Payment {
-
-}
--- payment/src/main/java/ca/ulaval/glo4002/payment/domaine/payment/PaymentRepository.java
+++
@@ -1,7 +0,0 @@
-package ca.ulaval.glo4002.payment.domaine.payment;
-
-public interface PaymentRepository {
-
-  public void SavePayment(Payment payment);
-
-}

331435c9d4aab1c61cea25b2aedaa9bb1e6a295b
zasas <zakaria.sassioui.1@ulaval.ca>
2017-11-12 21:00:20 +0100

    test Identity equals

--- payment/src/test/java/ca/ulaval/glo4002/payment/domain/identity/IdentityTest.java
+++ payment/src/test/java/ca/ulaval/glo4002/payment/domain/identity/IdentityTest.java
@@ -1,10 +1,10 @@
 package ca.ulaval.glo4002.payment.domain.identity;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
-
-import ca.ulaval.glo4002.payment.domain.identity.Identity;
 
 public class IdentityTest {
 
@@ -15,6 +15,22 @@ public class IdentityTest {
     identity = new Identity();
 
     assertNotNull(identity.getNumber());
+  }
+
+  @Test
+  public void givenDifferentIdentityWhenEqualsThenReturnsFalse() {
+    identity = new Identity(3L);
+    Identity differentIdentity = new Identity();
+
+    assertFalse(differentIdentity.equals(identity));
+  }
+
+  @Test
+  public void givenEqualsIdentityWhenEqualsThenReturnsTrue() {
+    identity = new Identity(3L);
+    Identity equalsIdentity = new Identity(3L);
+
+    assertTrue(equalsIdentity.equals(identity));
   }
 
 }

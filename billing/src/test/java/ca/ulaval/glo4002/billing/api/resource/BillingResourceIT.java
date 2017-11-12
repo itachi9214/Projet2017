268b9189f192795ebde13865a87adcc74b6d7466
marianne-butaye <marianne.butaye@gmail.com>
2017-11-12 13:51:25 -0500

    Added ExceptionMapperFactoryTest + updated BillTest

--- billing/src/test/java/ca/ulaval/glo4002/billing/api/resource/BillingResourceIT.java
+++ billing/src/test/java/ca/ulaval/glo4002/billing/api/resource/BillingResourceIT.java
@@ -16,7 +16,6 @@ import javax.ws.rs.core.Response.Status;
 
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.test.JerseyTest;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -30,7 +29,6 @@ import ca.ulaval.glo4002.billing.domain.submision.DueTerm;
 import ca.ulaval.glo4002.billing.domain.submision.NegativeParameterException;
 import ca.ulaval.glo4002.billing.domain.submision.OrderedProduct;
 import ca.ulaval.glo4002.billing.http.ClientNotFoundException;
-import ca.ulaval.glo4002.billing.http.ProductNotFoundException;
 import ca.ulaval.glo4002.billing.service.bill.BillService;
 import ca.ulaval.glo4002.billing.service.submission.SubmissionService;
 
@@ -80,41 +78,6 @@ public class BillingResourceIT extends JerseyTest {
     Response response = target("/bills").request().post(requestEntity);
 
     assertEquals(Status.CREATED.getStatusCode(), response.getStatus());
-  }
-
-  @Ignore
-  @Test
-  public void givenNonExistingClientWhenCreateSubmissionThenResponseStatusIsBadRequest() {
-    RequestSubmissionDto requestSubmissionDto = new RequestSubmissionDto(NON_EXISTING_CLIENT,
-        new Date(), DueTerm.DAYS30, new ArrayList<>());
-    Entity<RequestSubmissionDto> requestEntity = Entity.entity(requestSubmissionDto,
-        MediaType.APPLICATION_JSON);
-
-    Response response = target("/bills").request(MediaType.APPLICATION_JSON).post(requestEntity,
-        Response.class);
-
-    assertEquals(Status.BAD_REQUEST.getStatusCode(), response.getStatus());
-  }
-
-  @Ignore
-  @Test
-  public void givenNonExistingProductWhenCreateSubmissionThenResponseStatusIsBadRequest()
-      throws NegativeParameterException {
-    List<OrderedProduct> items = new ArrayList<>();
-    willThrow(new ProductNotFoundException(NON_EXISTING_PRODUCT)).given(submissionService)
-        .verifyProductsExist(items);
-
-    RequestSubmissionDto requestSubmissionDto = new RequestSubmissionDto(EXISTING_CLIENT,
-        new Date(), DueTerm.DAYS30, items);
-    Entity<RequestSubmissionDto> requestEntity = Entity.entity(requestSubmissionDto,
-        MediaType.APPLICATION_JSON);
-    willThrow(new ProductNotFoundException(NON_EXISTING_PRODUCT)).given(submissionService)
-        .createSubmission(requestSubmissionDto);
-
-    Response response = target("/bills").request(MediaType.APPLICATION_JSON).post(requestEntity,
-        Response.class);
-
-    assertEquals(Status.BAD_REQUEST.getStatusCode(), response.getStatus());
   }
 
 }
--- payment/src/test/java/ca/ulaval/glo4002/payment/domain/bill/BillTest.java
+++ payment/src/test/java/ca/ulaval/glo4002/payment/domain/bill/BillTest.java
@@ -9,9 +9,11 @@ import org.junit.Test;
 
 public class BillTest {
 
+  private static final double FLOAT_DIFFERENCE = 0.001;
   private static final BigDecimal REMAINING_AMOUNT = new BigDecimal(50);
   private static final long BILL_NUMBER = 1L;
-  private static final float AMOUNT = 25;
+  private static final float LOWER_AMOUNT = 25;
+  private static final float BIGGER_AMOUNT = 75;
 
   private Bill bill;
 
@@ -22,9 +24,23 @@ public class BillTest {
 
   @Test
   public void whenSubtractPaidAmountThenPaymentIsSubtracted() {
-    bill.substractPaidAmount(AMOUNT);
+    bill.substractPaidAmount(LOWER_AMOUNT);
 
-    assertEquals(new BigDecimal(AMOUNT), bill.getRemainingAmount());
+    assertEquals(new BigDecimal(LOWER_AMOUNT), bill.getRemainingAmount());
+  }
+
+  @Test
+  public void givenRemainingAmountLowerThanAmountWhenGetPriceThatCanBePaidThenReturnsRemainingAmount() {
+    float res = bill.getPriceThatCanBePaid(BIGGER_AMOUNT);
+
+    assertEquals(REMAINING_AMOUNT.floatValue(), res, FLOAT_DIFFERENCE);
+  }
+
+  @Test
+  public void givenRemainingAmountLowerBiggerThanAmountWhenGetPriceThatCanBePaidThenReturnsAmount() {
+    float res = bill.getPriceThatCanBePaid(LOWER_AMOUNT);
+
+    assertEquals(LOWER_AMOUNT, res, FLOAT_DIFFERENCE);
   }
 
 }
